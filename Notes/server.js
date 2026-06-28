afe6024acd023bdbb89cd60e33c006b339f13f7e
Dipanshu Singh <ds23322seven@gmail.com>
2026-06-28 10:03:16 +0000

    Backend Deployment

--- Notes/server.js
+++
@@ -1,6 +0,0 @@
-const app=require("./src/app")
-
-
-app.listen('4000',()=>{
-    console.log('server started')
-})
---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const port =4000
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+app.get('/setup',(req,res)=>{
+    res.send('<h1>Getting It</h1>')
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
